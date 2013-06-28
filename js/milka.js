917ea43be94edf27e246c99efc479c28cbbf2059
mbarisic <mlkbarisic@gmail.com>
2013-06-28 15:03:35 +0200

    uradjen domaci

---
+++ js/milka.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	//1.  dodati klasu green na sve stavke
+	//	$("li").addClass("green");
+
+	//2.  selektovati drugu stavu liste
+	//	$("li:eq(1)").css({"border":"1px solid red"});
+
+	//3.  ubaciti novi naslov posle paragrafa
+		//$("<h1>Naslov</h1>").insertAfter("p");
+
+	//4.  ubaciti novu podstavku na pocetak liste
+		//$("li > ul").prepend("<li>nova podstavka</li>");
+
+	//5.  dodati classu green na sve stavke osim na trecu
+		/*$("li").addClass("green");
+			$("li:eq(2)").removeClass("green");*/
+
+	//6.  izmeniti tekst u naslovu
+	//	$('h1').text("Promenjen tekst");
+
+	//7.  dodati novu recenicu iznad paragrafa
+	//	$('<p>nova recenica iznad paragrafa</p>').insertBefore("p");
+
+	});
+
